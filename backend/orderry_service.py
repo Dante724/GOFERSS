c1b1cd982ba230c9d99d4d0a57ad173d216e3fd3
Aryan <59132949+Dante724@users.noreply.github.com>
2026-08-25 15:49:53 -0400

    orderry_service.py

--- backend/orderry_service.py
+++ backend/orderry_service.py
@@ -4,7 +4,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-ORDERRY_API_KEY = os.environ.get("ORDERRY_API_KEY")
+ORDERRY_API_KEY = os.environ.get("7eba7fe43d1045a990b0a6fd85945f8e")
 
 def create_orderry_lead(name, phone, email, message):
     if not ORDERRY_API_KEY:
@@ -14,7 +14,7 @@ def create_orderry_lead(name, phone, email, message):
     url = "https://api.orderry.com/v2/lead/"
     
     headers = {
-        "Authorization": f"Bearer {ORDERRY_API_KEY}",
+        "Authorization": f"Bearer {7eba7fe43d1045a990b0a6fd85945f8e}",
         "Content-Type": "application/json"
     }
     
