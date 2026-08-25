d982e2713412e70dfbbe9f22b5f588809def5aaf
Aryan <59132949+Dante724@users.noreply.github.com>
2026-08-25 16:29:36 -0400

    orderry_service.py

--- backend/orderry_service.py
+++ backend/orderry_service.py
@@ -23,7 +23,7 @@ def create_orderry_lead(name, phone, email, message):
         "contact_phone": phone,
         "contact_email": email,
         "notes": message,
-        "lead_type_id": 1
+        "_notify_leadtype_id": 1
     }
     
     logger.info(f"Sending to Orderry: {payload}")
